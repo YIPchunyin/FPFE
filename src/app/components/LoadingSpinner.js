f75693cdb6c7d1238d4ff5bd4f2d952725e5e068
YIPchunyin <yejunyan66@gmail.com>
2024-11-24 19:48:04 +0800

    test2

--- src/app/components/LoadingSpinner.js
+++ src/app/components/LoadingSpinner.js
@@ -1,12 +1,13 @@
 'use client';
 import React from 'react';
-import './LoadingSpinner.css'; // 导入样式文件
+import Image from 'next/image'; // 如果使用 Next.js
+import logo from '@/app/public/logo.png';
 
 const LoadingSpinner = () => {
   return (
-    <div className="loading-container">
-      <div className="loading-spinner"></div>
-      <p className="loading-text">加载中，请稍候...</p>
+    <div className='flex justify-center max-w-3xl m-auto  items-center h-screen flex-col animate-blink'>
+        <Image width={500} height={500} src={logo} alt="Logo" className="" />
+        <p className='text-center text-4xl font-bold'>Loading...</p>
     </div>
   );
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import LoginForm from "./components/LoginForm";
 import RegisterForm from "@/app/components/RegisterForm";
 import { useEffect, useState } from "react";
 import { removeToken, getToken } from "./../utils/auth";
+// import logo from '@/app/public/logo2.jpg';
 import logo from '@/app/public/logo.png';
 import Image from "next/image";
 import creatpost from "@/app/public/createpost.png";
@@ -60,141 +61,143 @@ export default function RootLayout({ children }) {
   };
 
 
-    useEffect(() => {
-      const token = getToken();
-      if (token) {
-        const fetchUserDetails = async () => {
-          try {
-            const res = await post("/users/get-user-info", { token }, true);
-            if (res.status === 200) {
-              console.log("用户信息:", res);
-              setUserDetails(res.user);
-              setIsLoggedIn(true);
-              localStorage.setItem("userId", res.user._id);
-              console.log(res.user._id)
-            }
-          } catch (error) {
-            console.log("请求用户信息时发生错误:", error);
+  useEffect(() => {
+    const token = getToken();
+    if (token) {
+      const fetchUserDetails = async () => {
+        try {
+          const res = await post("/users/get-user-info", { token }, true);
+          if (res.status === 200) {
+            console.log("用户信息:", res);
+            setUserDetails(res.user);
+            setIsLoggedIn(true);
+            localStorage.setItem("userId", res.user._id);
+            console.log(res.user._id)
           }
-        };
-        fetchUserDetails();
-      }
-    }, [isLoggedIn]);
-
-    useEffect(() => {
-      const handleScroll = () => {
-        setIsVisible(window.scrollY > 600);
+        } catch (error) {
+          console.log("请求用户信息时发生错误:", error);
+        }
       };
+      fetchUserDetails();
+    }
+  }, [isLoggedIn]);
 
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-
-    const isOnCreatePostPage = pathname === '/createpost';
-
-    const scrollToTop = () => {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.scrollY > 600);
     };
 
-    return (
-      <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex items-center justify-center w-full py-2 border-b border-gray-300">
-            <div className="flex items-center justify-between w-4/5 mx-auto">
-              <div className="flex items-center">
-                <Link href="/">
-                  <Image src={logo} alt="Logo" width={50} height={50} className="rounded-full" />
-                </Link>
-              </div>
-              <div className="flex items-center flex-1 mx-5">
-                <input
-                  type="text"
-                  placeholder="搜索..."
-                  className="flex-1 px-3 py-2 border border-gray-300 rounded"
-                  value={searchKeyword}
-                  onChange={(e) => setSearchKeyword(e.target.value)}
-                />
-                <button onClick={handleSearch}>
-                  <Image src={searchIcon} alt="搜索" width={30} height={30} className="ml-3 cursor-pointer" />
-                </button>
-              </div>
-              <div className="flex">
-                {isLoggedIn ? (
-                  <div className="relative inline-block group flex items-center">
-                    <Link href={`/user/${userDetails._id}`} className="flex items-center">
-                      <Image
-                        src={userDetails.img_path || userimg} // 使用用户头像或默认头像
-                        alt={`${userDetails.username || '用户'}的头像`}
-                        width={50}
-                        height={50}
-                        //原型頭像
-                        className="cursor-pointer rounded-full object-cover"
-                      />
-                      {/* <span className="px-2 py-2 group-hover:cursor-pointer">{userDetails.name}</span> */}
-                    </Link>
-                    <div className="absolute hidden w-60 p-4 bg-white border border-gray-300 shadow-md mt-40 group-hover:block" style={{zIndex: 10000}}>
-                      <p>用户名: {userDetails.name}</p>
-                      <p>邮箱: {userDetails.email}</p>
-                      <p>身份: {userDetails.role}</p>
-                    </div>
-                    <button className="px-4 py-2 ml-2 text-white bg-blue-600 rounded" onClick={handleLogout}>
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const isOnCreatePostPage = pathname === '/createpost';
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <header className="flex items-center justify-center w-full py-2 border-b border-gray-300">
+          <div className="flex items-center justify-between w-4/5 mx-auto">
+            <div className="flex items-center">
+              <Link href="/">
+                <Image src={logo} alt="Logo" width={50} height={50} className="rounded-full" />
+              </Link>
+            </div>
+            <div className="flex items-center flex-1 mx-5">
+              <input
+                type="text"
+                placeholder="搜索..."
+                className="flex-1 px-3 py-2 border border-gray-300 rounded"
+                value={searchKeyword}
+                onChange={(e) => setSearchKeyword(e.target.value)}
+              />
+              <button onClick={handleSearch}>
+                <Image src={searchIcon} alt="搜索" width={30} height={30} className="ml-3 cursor-pointer" />
+              </button>
+            </div>
+            <div className="flex">
+              {isLoggedIn ? (
+                <div className="relative group flex items-center">
+                  <Link href={`/user/${userDetails._id}`} className="flex items-center">
+                    <Image
+                      src={userDetails.img_path || userimg} // 使用用户头像或默认头像
+                      alt={`${userDetails.username || '用户'}的头像`}
+                      width={50}
+                      height={50}
+                      //原型頭像
+                      className="cursor-pointer rounded-full object-cover"
+                    />
+                    {/* <span className="px-2 py-2 group-hover:cursor-pointer">{userDetails.name}</span> */}
+                  </Link>
+                  <div className="absolute hidden w-60 p-4 bg-white border border-gray-300 shadow-md mt-48 group-hover:block" style={{ zIndex: 10000 }}>
+                    <p>用户名: {userDetails.name}</p>
+                    <p>邮箱: {userDetails.email}</p>
+                    <p>身份: {userDetails.role}</p>
+                    <button className="p-2 bg-blue-600 text-white rounded" onClick={handleLogout}>
                       登出
                     </button>
                   </div>
 
-                ) : (
-                  <>
-                    <button className="px-4 py-2 ml-2 text-white bg-blue-600 rounded" onClick={() => setLoginOpen(true)}>
-                      登入
-                    </button>
-                    <button className="px-4 py-2 ml-2 text-white bg-blue-600 rounded" onClick={() => setRegisterOpen(true)}>
-                      注册
-                    </button>
-                  </>
-                )}
-              </div>
+                  {isLoggedIn && !isOnCreatePostPage && (
+                    <div className="cursor-pointer ml-3">
+                      <Link href="/createpost">
+                        <Image src={creatpost} alt="发帖" width={30} height={30} />
+                      </Link>
+                    </div>
+                  )}
+                </div>
+
+              ) : (
+                <>
+                  <button className="px-4 py-2 ml-2 text-white bg-blue-600 rounded" onClick={() => setLoginOpen(true)}>
+                    登入
+                  </button>
+                  <button className="px-4 py-2 ml-2 text-white bg-blue-600 rounded" onClick={() => setRegisterOpen(true)}>
+                    注册
+                  </button>
+                </>
+              )}
             </div>
-          </header>
+          </div>
+        </header>
 
-          {isLoginOpen && (
-            <LoginForm
-              isOpen={isLoginOpen}
-              onClose={() => setLoginOpen(false)}
-              onLoginSuccess={handleLoginSuccess}
-            />
-          )}
+        {isLoginOpen && (
+          <LoginForm
+            isOpen={isLoginOpen}
+            onClose={() => setLoginOpen(false)}
+            onLoginSuccess={handleLoginSuccess}
+          />
+        )}
 
-          {isRegisterOpen && (
-            <RegisterForm
-              onClose={() => setRegisterOpen(false)}
-              onLoginSuccess={handleLoginSuccess}
-            />
-          )}
+        {isRegisterOpen && (
+          <RegisterForm
+            onClose={() => setRegisterOpen(false)}
+            onLoginSuccess={handleLoginSuccess}
+          />
+        )}
 
-          {isLoggedIn && !isOnCreatePostPage && (
-            <div className="fixed bottom-16 right-5 p-2 cursor-pointer">
-              <Link href="/createpost">
-                <Image src={creatpost} alt="发帖" width={40} height={40} />
-              </Link>
-            </div>
-          )}
 
-          {children}
 
-          {isVisible && (
-            <button onClick={scrollToTop} className="fixed bottom-5 right-5 flex items-center justify-center w-12 h-12 bg-blue-600 text-white rounded-full transition-colors duration-300 hover:bg-blue-700">
-              ↑
-            </button>
-          )}
-        </body>
-      </html>
+        {children}
 
-    );
+        {isVisible && (
+          <button onClick={scrollToTop} className="fixed bottom-5 right-5 flex items-center justify-center w-12 h-12 bg-blue-600 text-white rounded-full transition-colors duration-300 hover:bg-blue-700">
+            ↑
+          </button>
+        )}
+      </body>
+    </html>
 
-  }
+  );
+
+}
 
