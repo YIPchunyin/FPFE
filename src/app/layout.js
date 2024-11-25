626432f8953835c5c24d757aae2f276e40d46f2c
YIPchunyin <yejunyan66@gmail.com>
2024-11-26 00:23:51 +0800

    test222222222222222222

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,16 +19,16 @@ import 'quill/dist/quill.snow.css';
 
 
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export default function RootLayout({ children }) {
   const [isLoginOpen, setLoginOpen] = useState(false);
